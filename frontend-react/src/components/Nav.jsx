ed099698ac2a73ac35db384fad11b6e351900717
sabdahutabarat2804 <sabdahutabarat2804@gmail.com>
2021-12-01 18:28:51 +0700

    membuat Navbar React Kafe

---
+++ frontend-react/src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <div className="Nav">
+      <div className="kiri">
+        <h2>React Kafe</h2>
+      </div>
+      <div className="kanan">
+        <ul>
+          <li>Cashier</li>
+          <li>Add</li>
+          <li>Edit</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
